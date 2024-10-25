355abce45a37105d920633bec5828258d4f68ead
Om <omshete0550@gmail.com>
2024-10-25 23:14:38 +0530

    updates

---
+++ client/app/(tabs)/UploadImg.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function UploadImg() {
+  return (
+    <View>
+      <Text>UploadImg</Text>
+    </View>
+  )
+}
---
+++ client/app/(tabs)/_layout.jsx
@@ -0,0 +1,51 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { Ionicons } from '@expo/vector-icons';
+import { AntDesign } from "@expo/vector-icons";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        // tabBarActiveTintColor: Colors.green,
+        tabBarStyle: {
+        //   backgroundColor: Colors.GRAY,
+          borderRadius: 20,
+          position: "absolute",
+          // bottom: 10,
+          // left: 30,
+          // right: 30,
+          height: 75,
+          paddingBottom: 10,
+          paddingTop: 10,
+          elevation: 5,
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: 5 },
+          shadowOpacity: 0.3,
+          shadowRadius: 10,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="Home"
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="UploadImg"
+        options={{
+          tabBarLabel: "Category",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="list" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
